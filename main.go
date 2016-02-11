a45067f6409a58345424b70c8e8e8b47013d6bf3
Alexander Stensrud <alex@stensrud.nu>
2016-02-11 20:24:08 +0100

    Profiling

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"net/http"
+	"net/http/pprof"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/gorilla/mux"
@@ -11,12 +12,19 @@ import (
 var (
 	clientDir = flag.String("client", "./client", "")
 	listen    = flag.String("listen", ":3000", "")
+	profile   = flag.Bool("profile", false, "")
 )
 
 func main() {
 	flag.Parse()
 
 	router := mux.NewRouter()
+
+	if *profile {
+		router.Handle("/debug/profile", http.HandlerFunc(pprof.Profile))
+		router.Handle("/debug/heap", pprof.Handler("heap"))
+		router.Handle("/debug/trace", http.HandlerFunc(pprof.Trace))
+	}
 
 	router.PathPrefix("/").HandlerFunc(staticHandler(*clientDir)).Methods("HEAD", "GET")
 
