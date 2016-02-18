7079b07806360f697539c52075534a4da9aaa2b5
Alexander Stensrud <alex@stensrud.nu>
2016-02-18 09:50:41 +0100

    Reverse proxy for path prefixes

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"net/http"
+	"net/http/httputil"
 	"net/http/pprof"
+	"net/url"
 	"path/filepath"
+	"strings"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/gorilla/mux"
@@ -13,6 +16,7 @@ import (
 var (
 	clientDir = flag.String("client", "./client", "")
 	listen    = flag.String("listen", ":3000", "")
+	proxy     = flag.String("proxy", "", "{prefix},{url};...")
 	profile   = flag.String("profile", "", "")
 )
 
@@ -20,6 +24,28 @@ func main() {
 	flag.Parse()
 
 	router := mux.NewRouter()
+
+	if *proxy != "" {
+		proxies := strings.Split(*proxy, ";")
+
+		for _, proxy := range proxies {
+			parts := strings.Split(proxy, ",")
+			if len(parts) < 2 {
+				logrus.Fatalf("Proxy syntax")
+			}
+
+			url, err := url.Parse(parts[1])
+			if err != nil {
+				logrus.Fatalf("Proxy syntax")
+			}
+
+			logrus.Infof("Proxy %s -> %s", parts[0], url)
+
+			router.PathPrefix(parts[0]).Handler(
+				httputil.NewSingleHostReverseProxy(url),
+			)
+		}
+	}
 
 	if *profile != "" {
 		prof := func(name string) string { return filepath.Join("/", *profile, name) }
